d0827668bf8666bcf2f4748d0f8ba4e832b10b10
Dominik Tran <tran@cesnet.cz>
2023-02-01 13:36:19 +0100

    update paths for v3.02

--- lbr_trex_client/__init__.py
+++ lbr_trex_client/__init__.py
@@ -2,4 +2,4 @@ import os
 import sys
 
 # Set TREX_EXT_LIBS which is used by some TRex modules
-os.environ['TREX_EXT_LIBS'] = os.path.join(os.path.dirname(__file__), "v2_86/external_libs")
+os.environ['TREX_EXT_LIBS'] = os.path.join(os.path.dirname(__file__), "v3_02/external_libs")
